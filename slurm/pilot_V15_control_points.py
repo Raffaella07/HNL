f91b92e72c8158452534379e89066b98238d64c6
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-12-10 18:55:53 +0100

    update

--- slurm/pilot_V15_control_points.py
+++ slurm/pilot_V15_control_points.py
@@ -5,7 +5,7 @@ from python.common  import Point,Config,getCtauEff
 
 
 p = Point(mass=999,ctau=999,vv=999)
-cfg = Config(nevtseff=2000,muoneff=3.80e-02,displeff=1.0,timeevt=230,timejob=16,contingency=5.)
+cfg = Config(nevtseff=2000,muoneff=3.80e-02,displeff=1.0,timeevt=125,timejob=16,contingency=5.)
 #cfg = Config(nevtseff=5,muoneff=1.,displeff=1.0,timeevt=100,timejob=01,contingency=3.)
 p.setConfig(cfg)
 
