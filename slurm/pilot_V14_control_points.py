5797b5f6ea8fe22a23fd359b4a0411d3d444c5dd
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-12-17 12:29:01 +0100

    latest batches of points

--- slurm/pilot_V14_control_points.py
+++ slurm/pilot_V14_control_points.py
@@ -4,8 +4,8 @@ from python.common  import Point,Config,getCtauEff
 #   - N.B.: mass must be a float
 
 p = Point(mass=999,ctau=999,vv=999)
-#cfg = Config(nevtseff=2000,muoneff=2.0e-02,displeff=1.0,timeevt=100,timejob=16,contingency=5.)
-cfg = Config(nevtseff=10,muoneff=1.,displeff=1.0,timeevt=100,timejob=01,contingency=3.)
+cfg = Config(nevtseff=5,muoneff=5.0e-03,displeff=1.0,timeevt=200,timejob=1,contingency=3.)
+#cfg = Config(nevtseff=5,muoneff=1.,displeff=1.0,timeevt=100,timejob=01,contingency=3.)
 p.setConfig(cfg)
 
 points = []
